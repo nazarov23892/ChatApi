1d0be4da6e62c60a2a5aec38b13c092fd08c91ac
man238 <man23892@gmail.com>
2023-06-30 11:20:05 +0500

    added chat service tests

---
+++ ChatApi.Tests/ChatService_CreateChat_Tests.cs
@@ -0,0 +1,48 @@
+﻿using ChatApi.BLL.Entities;
+using ChatApi.BLL.Repositories;
+using ChatApi.BLL.Services.Chats.Concrete;
+using ChatApi.BLL.Services.Chats.DTOs;
+using ChatApi.BLL.Services.Users.Concrete;
+using ChatApi.BLL.Services.Users.DTOs;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApi.Tests
+{
+    public class ChatService_CreateChat_Tests
+    {
+        [Fact]
+        public void Cannot_Create_Chat_When_Chatrname_Is_Empty()
+        {
+            // arrange
+
+            Mock<IChatRepository> mock = new Mock<IChatRepository>();
+
+            ChatService target = new ChatService(mock.Object);
+
+            // act
+
+            CreateChatResponseDto? result = target.Create(new CreateChatRequestDto
+            {
+                Name = string.Empty,
+                Users = new[] { "aaa", "bbb" }
+            });
+
+            // assert
+
+            Assert.Null(result);
+            Assert.True(target.HasValidationProblems);
+            Assert.Single(target.ValidationProblems);
+            Assert.Contains(
+                expectedSubstring: "Name field is required",
+                actualString: target.ValidationProblems.Keys.Single());
+            mock.Verify(
+                expression: m => m.Add(It.IsAny<Chat>()),
+                times: Times.Never);
+        }
+    }
+}
