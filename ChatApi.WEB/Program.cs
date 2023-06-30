df6bb51bbfc1a7fd8b67cc629480b831cdb68f1e
man238 <man23892@gmail.com>
2023-06-30 08:15:40 +0500

    Add project files.

---
+++ ChatApi.WEB/Program.cs
@@ -0,0 +1,15 @@
+namespace ChatApi.WEB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
