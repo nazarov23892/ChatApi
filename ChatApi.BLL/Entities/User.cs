a33f1dc5646c5fd05fccade8854b57d7ddc89356
man238 <man23892@gmail.com>
2023-06-30 08:17:34 +0500

    added user entity

---
+++ ChatApi.BLL/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApi.BLL.Entities
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
