e5321eb3a78702dfe379d7c250d886c01b4c8e3e
man238 <man23892@gmail.com>
2023-06-30 08:20:44 +0500

    added user service

---
+++ ChatApi.BLL/Services/Users/DTOs/CreateUserResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApi.BLL.Services.Users.DTOs
+{
+    public class CreateUserResponseDto
+    {
+        public string UserId { get; set; } = string.Empty;
+    }
+}
---
+++ ChatApi.BLL/Services/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using ChatApi.BLL.Services.Users.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApi.BLL.Services.Users
+{
+    public interface IUserService
+    {
+        CreateUserResponseDto? CreateUser(CreateUserRequestDto createUserDto);
+    }
+}
