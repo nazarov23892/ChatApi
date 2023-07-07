d513a3975b5da9e8d3e1cd6d4f50a3e31be933c6
man238 <man23892@gmail.com>
2023-07-07 11:05:04 +0500

    chat service: added tests for post message

--- ChatApi.Tests/ChatService_PostMessage_Tests.cs
+++ ChatApi.Tests/ChatService_PostMessage_Tests.cs
@@ -240,5 +240,68 @@ namespace ChatApi.Tests
                 times: Times.Never
                 );
         }
+
+        [Fact]
+        public void Cannot_PostMessage_When_Chat_Does_Not_Contain_User()
+        {
+            // arrange
+
+            Mock<IChatRepository> chatRepositoryMock = new Mock<IChatRepository>();
+            Mock<IUserRepository> userRepositoryMock = new Mock<IUserRepository>();
+
+            userRepositoryMock
+                .Setup(m => m.GetUser(It.IsAny<string>()))
+                .Returns<string>(valueFunction: userId => userId.Equals("03")
+                    ? new User
+                    {
+                        UserId = "03",
+                    }
+                    : null);
+
+            chatRepositoryMock
+                .Setup(m => m.GetChatWithUsers(It.IsAny<string>()))
+                .Returns<string>(valueFunction: chatId => chatId.Equals("01")
+                    ? new Chat
+                    {
+                        ChatId = "01",
+                        Users = new List<User> 
+                        { 
+                            new User { UserId = "01" }, 
+                            new User { UserId = "02" } 
+                        }
+                    }
+                    : null);
+
+            ChatService target = new ChatService(
+                chatRepositoryMock.Object,
+                userRepositoryMock.Object);
+
+            PostMessageRequestDto postMessageRequest = new PostMessageRequestDto
+            {
+                Author = "03",
+                Chat = "01",
+                Text = "text"
+            };
+
+            // act
+
+            PostMessageResponse? response = target.PostMessage(postMessageRequest);
+
+            // assert
+
+            Assert.Null(response);
+            Assert.True(target.HasValidationProblems);
+            Assert.Single(target.ValidationProblems);
+            Assert.Contains(
+                expectedSubstring: "chat does not have this user",
+                actualString: target.ValidationProblems.Single(),
+                comparisonType: StringComparison.OrdinalIgnoreCase
+                );
+
+            chatRepositoryMock.Verify(
+                expression: m => m.AddMessage(It.IsAny<Message>()),
+                times: Times.Never
+                );
+        }
     }
 }
